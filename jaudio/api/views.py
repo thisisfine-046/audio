71ce5d64e04dd1ae51ed24c1d9033e3eb2fdf438
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-02-28 16:05:53 +0700

    Django & React Tutorial #1

--- jaudio/api/views.py
+++ jaudio/api/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def main(resquest):
-    return HttpResponse("<h2>jdoa</h2>")
+    return HttpResponse("<h1>jdoa</h1>")
