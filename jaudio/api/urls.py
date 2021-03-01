99302e834b4889ef13a3c9c8dfae871ce24c5dc1
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-03-01 23:42:47 +0700

    Django & React Tutorial #2 - Django REST Framework

--- jaudio/api/urls.py
+++ jaudio/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path ,include
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('',main)
-]
+    path('home',RoomView.as_view())
+] 
--- jaudio/api/views.py
+++ jaudio/api/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializers
 # Create your views here.
-def main(resquest):
-    return HttpResponse("<h2>jdoa</h2>")
+
+# CreateAPIView để tạo
+# ListAPIView để xem
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializers
+
+
+
