518425d9d8cfce2f0ead6c13a38b8e54a697acdc
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-03-02 22:38:27 +0700

    Django & React Tutorial #5 - Handling POST Requests (Django REST)

--- jaudio/api/serializers.py
+++ jaudio/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Room
 
-class RoomSerializers(serializers.ModelSerializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id','code','host','guest_can_pause'
                     ,'votes_to_skip', 'create_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause','votes_to_skip')
--- jaudio/api/urls.py
+++ jaudio/api/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path ,include
-from .views import RoomView
+from .views import RoomView , CreateRoomView
 
 urlpatterns = [
-    path('room',RoomView.as_view())
+    path('room',RoomView.as_view()),
+    path('createroom',CreateRoomView.as_view())
 ] 
