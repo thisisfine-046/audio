b158a878cd02495e332386de34fa71fa4563e54f
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-03-03 21:20:16 +0700

    Django & React Tutorial #7 - Calling API Endpoints From React

--- jaudio/frontend/src/components/HomePage.js
+++ jaudio/frontend/src/components/HomePage.js
@@ -2,7 +2,7 @@ import React , { Component } from 'react';
 import RoomJoinPage from "./RoomJoinPage"
 import CreateRoomPage from "./CreateRoomPage"
 import IndexPage from "./IndexPage"
-
+import Room from "./Room"
 import { 
     BrowserRouter as Router,
     Switch, 
@@ -26,6 +26,7 @@ export default class HomePage extends Component{
                 
                 <Route path="/join" component={RoomJoinPage} />
                 <Route path="/create" component={CreateRoomPage} />
+                <Route path ="/room/:roomCode" component = {Room}/>
             </switch>
         </Router>
     }
