9ff5e7b9a206a66b2327c5a53730b145f87cc029
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-03-03 11:04:53 +0700

    Create Frontend folder

--- jaudio/frontend/src/components/App.js
+++ jaudio/frontend/src/components/App.js
@@ -14,7 +14,6 @@ export default class App extends Component{
         return (
             <div>
                 <HomePage />
-
             </div>
         );
     }
--- jaudio/frontend/src/components/HomePage.js
+++ jaudio/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React , { Component } from 'react';
 import RoomJoinPage from "./RoomJoinPage"
 import CreateRoomPage from "./CreateRoomPage"
+
 import { 
     BrowserRouter as Router,
     Switch, 
@@ -19,10 +20,8 @@ export default class HomePage extends Component{
     render(){
         return <Router>
             <switch>
-                <Route exact path = '/'>
-                    <p>
-                        This is the Home Page
-                    </p>
+                <Route exact path = "/">
+                    <h1>Home Room</h1>
                 </Route>
                 <Route path="/join" component={RoomJoinPage} />
                 <Route path="/create" component={CreateRoomPage} />
