46ad6ee9964e2e2a5290d3d7959901456dd2a85f
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-03-25 21:07:50 +0700

    update room

--- jaudio/frontend/src/components/App.js
+++ jaudio/frontend/src/components/App.js
@@ -69,7 +69,7 @@ particlesJS("particles-js", {
       },
       modes: {
         grab: { distance: 140, line_linked: { opacity: 1 } },
-        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 2 },
         repulse: { distance: 200, duration: 0.4 },
         push: { particles_nb: 4 },
         remove: { particles_nb: 2 }
--- jaudio/frontend/src/components/HomePage.js
+++ jaudio/frontend/src/components/HomePage.js
@@ -16,12 +16,26 @@ from "react-router-dom";
 export default class HomePage extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            roomCode: null,
+        };
     }
+
+    async componentDidMount() {
+        fetch("/api/user-in-room")
+          .then((response) => response.json())
+          .then((data) => {
+            this.setState({
+              roomCode: data.code,
+            });
+          });
+    }
+    
 
     render(){
         return <Router>
             <switch>
-                <Route exact path = "/" component={IndexPage} />            
+                <Route exact path = "/"  component={IndexPage}/>        
                 <Route path="/join" component={RoomJoinPage} />
                 <Route path="/create" component={CreateRoomPage} />
                 <Route path ="/room/:roomCode" component = {Room}/>
