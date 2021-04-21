47d2c3047d82650390551ce9f739c701f4ce829a
KhangLe1412 <lekhang.1412@gmail.com>
2021-04-21 20:49:37 +0700

    Update

--- jaudio/frontend/src/components/Login.js
+++ jaudio/frontend/src/components/Login.js
@@ -39,58 +39,52 @@ export default class LoginPage extends Component{
 
     renderSpotifyLoginButton(){
         return (
-            <div class="container" id="container">
-        
-            <div class="form-container sign-up-container">
-                <form action="#">
-                    <h1>Create Account</h1>
-                    <button>Sign Up</button>
-                </form>
-            </div>
+            <div>
+                <div class="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Create Account</h1>
 
-            <div class="form-container sign-in-container">
-                <form action="#">
-                    <h1>Sign in</h1>
-
-                    <button 
-                    onClick={() => this.toGetSpotifyAuth()}>
-                        Sign In
-                        
-                    </button>
-                </form>
-            </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn" >Sign In</button>
-                    </div>
-                    <div class="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
+                        <span>Sign up with your email address</span>
+                        <div class="social-container">
+                        <a href="https://www.spotify.com/us/signup/" class="spotify"><i class="fab fa-spotify"></i></a>
+                        <a href="https://soundcloud.com/signin" class="soundcloud"><i class="fab fa-soundcloud"></i></a>
+                        <a href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp" class="youtube"><i class="fab fa-youtube"></i></a>
+                        </div>
+                        <span>We may use your email and devices for updates and tips on SoundCloud's, Spotify's and YouTube's products and services, and for activities notifications. You can unsubscribe for free at any time in your notification settings.</span>
+                    </form>
+                </div>
+                <div class="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
+                        <span>Log in with your email address</span>
+                        <div class="social-container">
+                        <button  onClick={() => this.toGetSpotifyAuth()} class="spotify"><i class="fab fa-spotify"></i></button>
+                        <button href="#" class="soundcloud"><i class="fab fa-soundcloud"></i></button>
+                        <button href="#" class="youtube"><i class="fab fa-youtube"></i></button>
+                        </div>
+                        <span>We may use your email and devices for updates and tips on SoundCloud's, Spotify's and YouTube's products and services, and for activities notifications. You can unsubscribe for free at any time in your notification settings.</span>
+                    </form>
+                </div>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button class="ghost" id="signIn" >Sign In</button>
+                        </div>
+                        <div class="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button class="ghost" id="signUp">Sign Up</button>
+                        </div>
                     </div>
                 </div>
+                
             </div>
-        </div>
         );
     }
 
     render(){
         return this.renderSpotifyLoginButton();
     }
-}
-window.onload=function(){
-	const signUpButton = document.getElementById('signUp');
-	const signInButton = document.getElementById('signIn');
-	const container = document.getElementById('container');
-	
-	
-	signInButton.addEventListener('click', () => {
-		container.classList.remove("right-panel-active");
-	});
-    signUpButton.addEventListener('click', () => {
-		container.classList.add("right-panel-active");
-	});s
 }
