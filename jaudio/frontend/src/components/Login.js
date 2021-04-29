411fd5080b6e0cd0900c28f4ef2cbd9a4b958f00
Triệu Phát Nguyễn <yisthemilkgone@gmail.com>
2021-04-29 15:56:10 +0700

    h

--- jaudio/frontend/src/components/Login.js
+++ jaudio/frontend/src/components/Login.js
@@ -15,7 +15,7 @@ import {
 
 
 const AUTH_URL =
-"https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+"https://accounts.spotify.com/authorize?client_id=c83a7a91bb4743aaaaae481d65b7debd&response_type=code&redirect_uri=http://127.0.0.1:8000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default class LoginPage extends Component{
     constructor(props){
